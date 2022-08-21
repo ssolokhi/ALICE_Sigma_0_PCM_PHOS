af1183215b830c7ab7b9310532f87ed8a4a44c3b
ssolokhi <solokhin02@gmail.com>
2022-08-22 00:05:51 +0300

    Minor fixes of the photon covertion method retriever

--- AliCaloResponse.cxx
+++ AliCaloResponse.cxx
@@ -35,10 +35,10 @@
 #include "AliV0ReaderV1.h"
 
 AliCaloResponse::AliCaloResponse(): AliAnalysisTaskSE(), fAOD(nullptr), fOutputList(nullptr), 
-fPIDResponse(nullptr),fV0ReaderV1(nullptr) {}
+fPIDResponse(nullptr) {}
 
 AliCaloResponse::AliCaloResponse(const char *name): AliAnalysisTaskSE(name), fAOD(nullptr), fOutputList(nullptr),
-fPIDResponse(nullptr), fV0ReaderV1(nullptr) {
+fPIDResponse(nullptr) {
 	DefineInput(0, TChain::Class());
 	DefineOutput(1, TList::Class());
 }
@@ -52,17 +52,12 @@ void AliCaloResponse::UserCreateOutputObjects() {
 	fOutputList->SetOwner(kTRUE);
 
 	fOutputList->Add(new TH1F("hSelectedEvents", "Number Of Selected Events", 5, 0, 5));
-
 	fOutputList->Add(new TH1F("hEventPt", "Event Transverse Momentum", 100, 0, 5));
-
 	fOutputList->Add(new TH1F("hVertexZ", "Vertex Z-Coordinate", 100, -20, 20));
-
 	fOutputList->Add(new TH1F("hClusterEnergy", "Cluster Energy", 100, 0, 25));
-
 	fOutputList->Add(new TH1F("hReconstructedPhotons", "Number Of Reconstructed Photons", 25, 0, 25));
-
+	
 	fOutputList->Add(new TH2F("hTPCResponse", "TPC Response", 100, 0, 4, 250, 0, 250));
-
 	fOutputList->Add(new TH2F("hElectronSignal", "Electron Signal", 100, 0, 4, 100, -10, 10));
 
 	PostData(1, fOutputList);
@@ -95,7 +90,6 @@ void AliCaloResponse::UserExec(Option_t *option) {
 		if (electronSignal > 5) continue;
 		FillHistogram("hElectronSignal", track->Pt(), electronSignal);
 		FillHistogram("hSelectedEvents", 2);
-
 	}
 
 	GetPHOSSignal();
@@ -118,23 +112,25 @@ void AliCaloResponse::GetPHOSSignal() {
 }
 
 void AliCaloResponse::GetPhotonConversionSignal() {
-	TString photonCutNumberV0Reader = "060000084001001500000000";
-	TString cutNumberPhoton = photonCutNumberV0Reader.Data();
-	TString cutNumberEvent = "00000003";
+	const TString cutNumberPhoton = "060000084001001500000000";
+	const TString cutNumberEvent = "00000003";
 	TString fV0ReaderV1Name = Form("V0ReaderV1_%s_%s", cutNumberEvent.Data(), cutNumberPhoton.Data());
 	AliV0ReaderV1 *fV0ReaderV1 = reinterpret_cast<AliV0ReaderV1*>(AliAnalysisManager::GetAnalysisManager()->GetTask(fV0ReaderV1Name.Data()));
-	//if (!fV0ReaderV1) *fV0ReaderV1 = new AliV0ReaderV1(fV0ReaderV1Name.Data());
-	FillHistogram("hReconstructedPhotons", 1);
+
+	if(fV0ReaderV1) {
+		FillHistogram("hReconstructedPhotons", fV0ReaderV1->GetNumberOfPrimaryTracks());
+		FillHistogram("hSelectedEvents", 5);
+	}
 }
 
-void AliCaloResponse::FillHistogram(const TString key, const double value) {
+void AliCaloResponse::FillHistogram(const TString &key, const double &value) {
 	TObject *obj = fOutputList->FindObject(key);
-	if (obj) dynamic_cast<TH1F*>(obj)->Fill(value);
+	if (obj) static_cast<TH1F*>(obj)->Fill(value);
 }
 
-void AliCaloResponse::FillHistogram(const TString key, const double value_x, const double value_y) {
+void AliCaloResponse::FillHistogram(const TString &key, const double &value_x, const double &value_y) {
 	TObject *obj = fOutputList->FindObject(key);
-	if (obj) dynamic_cast<TH2F*>(obj)->Fill(value_x, value_y);
+	if (obj) static_cast<TH2F*>(obj)->Fill(value_x, value_y);
 }
 
 void AliCaloResponse::Terminate(Option_t *option) {}
